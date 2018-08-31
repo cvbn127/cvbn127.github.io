1e39de66e79c1a5fa8f1e639e73844c7d369a175
Александр Мулика <cvbn127@gmail.com>
2018-09-01 00:07:45 +0300

    another step

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-// import { Papa } from "./papaparse.js";
 var Papa = Window.Papa;
 
 function handleFileSelect(evt) {
     evt.stopPropagation();
     evt.preventDefault();
 
-    var files = evt.dataTransfer.files; // FileList object.
-
-// files is a FileList of File objects. List some properties.
-    // var output = [];
-    // for (var i = 0, f; f = files[i]; i++) {
-    //     output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-    //                 f.size, ' bytes, last modified: ',
-    //                 f.lastModifiedDate.toLocaleDateString(), '</li>');
-    // }
-    // gets only the first file
+    var files = evt.dataTransfer.files;
     var csvFile = files[0];
     var parsedData = new Array;
     var header = new Array;
@@ -24,9 +14,9 @@ function handleFileSelect(evt) {
             console.log("Finished:", results);
             header = results.meta.fields;
             parsedData = results.data;
+            showFileInfo(header);
         }
     });
-    // console.log(header);
 }
 
 function handleDragOver(evt) {
@@ -34,6 +24,96 @@ function handleDragOver(evt) {
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
+
+function showFileInfo(header){
+    let fileInfoZone = document.createElement("div");
+    fileInfoZone.className = "row";
+
+    let leftBlock = document.createElement("div");
+    leftBlock.className = "horizontalBlock";
+    leftBlock.style = "width:30%;";
+    
+    let rightBlock = document.createElement("div");
+    rightBlock.className = "horizontalBlock";
+    rightBlock.style = "width:50%;";
+    rightBlock.textContent = "Plot";
+
+    let headerElementZone = document.createElement("div");
+    headerElementZone.className = "headerElementZone";
+    let indexElement = makeMovableHeaderElement("Index");
+    headerElementZone.appendChild(indexElement);
+    for(var i = 0; i < header.length; i++)
+    {
+        let element = makeMovableHeaderElement(header[i]);
+        headerElementZone.appendChild(element);
+    }
+    
+    leftBlock.appendChild(headerElementZone);
+
+    fileInfoZone.appendChild(leftBlock);
+    fileInfoZone.appendChild(rightBlock);
+
+    let dropZone = document.getElementById("drop_zone");
+    dropZone.hidden = true;
+    dropZone.parentNode.insertBefore(fileInfoZone,dropZone.nextSibling);
+}
+
+function addPlot(data){
+
+}
+
+function makeMovableHeaderElement(text){
+    let headerElement = document.createElement("div");
+    headerElement.className = "headerElement";
+    headerElement.textContent = text;
+}
+
+//Make the DIV element draggagle:
+// dragElement(document.getElementById("mydiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+
 
 // Setup the dnd listeners.
 var dropZone = document.getElementById('drop_zone');
